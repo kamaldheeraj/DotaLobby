9f8f77b67d88e60614a91a5841a461763a046801
Kamal Dandamudi <kamal.dandamudi@vensi.com>
2016-05-16 13:34:38 -0500

    Initial Commit

---
+++ DotaLobby/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DotaLobby
+//
+//  Created by Vensi Developer on 5/16/16.
+//  Copyright © 2016 SillyApps. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
